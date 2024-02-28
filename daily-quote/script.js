21245d52b4a18279270bdfabd2745d583a1e6804
OnaFlorent <bflorent53170@gmail.com>
2024-02-28 09:54:03 +0100

    update and correction button tweeter seventh exercise

--- daily-quote/script.js
+++ daily-quote/script.js
@@ -13,5 +13,5 @@ async function getQuote(url){
 getQuote(api_url);
 
 function tweet(){
-    window.open("https://twitter.com/intent/tweet?text=Hello%20world", "Tweet Window", "width=600, height=300");
+    window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + " ---- by " + author.innerHTML, "Tweet Window", "width=600, height=300");
 }
